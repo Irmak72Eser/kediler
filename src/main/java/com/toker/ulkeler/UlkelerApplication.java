9e824d4e479034da5609f3d779e053c761f12ce6
Öztürk Toker <105295131+Irmak72Eser@users.noreply.github.com>
2024-01-11 22:22:22 +0300

    gite dosyalar gönderildi yani commit edildi

---
+++ src/main/java/com/toker/ulkeler/UlkelerApplication.java
@@ -0,0 +1,13 @@
+package com.toker.ulkeler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UlkelerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UlkelerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toker/ulkeler/UlkelerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toker.ulkeler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UlkelerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
